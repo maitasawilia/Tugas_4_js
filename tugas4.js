b3d98d9584e7f697a81ff7649a87ef5e8f01ff25
maitasawilia <maitasaw@gmail.com>
2020-06-23 22:34:46 +0700

    Tugas 4 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas4.js
@@ -0,0 +1,29 @@
+var a = 170;
+var b = 160;
+var c = 150;
+
+if(c>b){
+  if(c>a){
+    console.log("A adalah yang paling rendah");
+  }else{
+    console.log("Nilai a adalah yang rendah kedua");
+  }
+}else{
+  if(c<a){
+    console.log("A adalah yang paling tinggi");
+  }else{
+    console.log("Nilai a adalah yang tinggi kedua");
+  }
+}
+
+
+
+// var a = 170;
+// var b = 160;
+// var c = 150;
+// if(a<b)
+// console.log("a lebih tinggi dari b")
+// if (b>c)
+// console.log("b lebih tinggi dari c")
+// if (c<a)
+// console.log("c lebih rendah dari a dan b")
